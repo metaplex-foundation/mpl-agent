3fa743e60ae1d11a0a1373197af21c97e43c638a
stranzhay <16522636+stranzhay@users.noreply.github.com>
2026-04-16 16:02:55 -0400

    chore: bump identity and tools to solana 3.0

--- clients/rust-identity/src/generated/accounts/agent_identity_v2.rs
+++ clients/rust-identity/src/generated/accounts/agent_identity_v2.rs
@@ -32,17 +32,17 @@ const AGENT_IDENTITY_V2_AGENT_TOKEN_FIXED_SIZE_OPTION_SENTINEL: [u8; 32] = [
 ];
 
 impl BorshSerialize for AgentIdentityV2 {
-    fn serialize<W: borsh::maybestd::io::Write>(
+    fn serialize<W: borsh::io::Write>(
         &self,
         writer: &mut W,
-    ) -> borsh::maybestd::io::Result<()> {
+    ) -> borsh::io::Result<()> {
         BorshSerialize::serialize(&self.key, writer)?;
         BorshSerialize::serialize(&self.bump, writer)?;
         BorshSerialize::serialize(&self.padding, writer)?;
         BorshSerialize::serialize(&self.asset, writer)?;
         match &self.agent_token {
             Some(value) => BorshSerialize::serialize(value, writer)?,
-            None => borsh::maybestd::io::Write::write_all(
+            None => borsh::io::Write::write_all(
                 writer,
                 &AGENT_IDENTITY_V2_AGENT_TOKEN_FIXED_SIZE_OPTION_SENTINEL,
             )?,
@@ -53,16 +53,16 @@ impl BorshSerialize for AgentIdentityV2 {
 }
 
 impl BorshDeserialize for AgentIdentityV2 {
-    fn deserialize_reader<R: borsh::maybestd::io::Read>(
+    fn deserialize_reader<R: borsh::io::Read>(
         reader: &mut R,
-    ) -> borsh::maybestd::io::Result<Self> {
+    ) -> borsh::io::Result<Self> {
         let key = BorshDeserialize::deserialize_reader(reader)?;
         let bump = BorshDeserialize::deserialize_reader(reader)?;
         let padding = BorshDeserialize::deserialize_reader(reader)?;
         let asset = BorshDeserialize::deserialize_reader(reader)?;
         let agent_token = {
             let mut buffer = [0u8; 32];
-            borsh::maybestd::io::Read::read_exact(reader, &mut buffer)?;
+            borsh::io::Read::read_exact(reader, &mut buffer)?;
             if buffer == AGENT_IDENTITY_V2_AGENT_TOKEN_FIXED_SIZE_OPTION_SENTINEL {
                 None
             } else {
