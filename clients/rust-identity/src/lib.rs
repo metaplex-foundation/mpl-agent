5b0848d27981e0456acc24e61905b9d281dcab06
stranzhay <16522636+stranzhay@users.noreply.github.com>
2026-04-17 17:52:01 -0400

    Revert "fix(rust-identity): support kinobi maybestd borsh paths"
    This reverts commit c070b62353b1a28aa95491812a7afdbdd55566b8.

--- clients/rust-identity/src/lib.rs
+++ clients/rust-identity/src/lib.rs
@@ -1,13 +1,3 @@
-extern crate self as borsh;
-
-pub use ::borsh_dep::*;
-
-pub mod maybestd {
-    pub mod io {
-        pub use ::borsh_dep::io::*;
-    }
-}
-
 mod generated;
 
 pub use generated::programs::MPL_AGENT_IDENTITY_ID as ID;
